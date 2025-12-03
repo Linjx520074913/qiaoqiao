90042a79ef61a967111c88728eea879e4fb0c3b9
linjingxiong <520074913@qq.com>
2025-12-03 17:53:49 +0800

    Initial Commit

---
+++ bill/billApp.swift
@@ -0,0 +1,17 @@
+//
+//  billApp.swift
+//  bill
+//
+//  Created by linjx on 2025/12/3.
+//
+
+import SwiftUI
+
+@main
+struct billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ billTests/billTests.swift
@@ -0,0 +1,17 @@
+//
+//  billTests.swift
+//  billTests
+//
+//  Created by linjx on 2025/12/3.
+//
+
+import Testing
+@testable import bill
+
+struct billTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
